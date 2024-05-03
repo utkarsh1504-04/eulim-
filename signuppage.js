6a1078b703370e81560cf3257d11ece2179b1847
asiduki <147263246+asiduki@users.noreply.github.com>
2024-05-03 20:01:55 +0530

    faltu

--- signuppage.js
+++ signuppage.js
@@ -1,10 +1,13 @@
-const signInBtnLink = document.querySelector('.signInBtn-link');
-const signUpBtnLink = document.querySelector('.signUpBtn-link');
-const wrapper = document.querySelector('.wrapper');
-signUpBtnLink.addEventListener('click', () => {
-    wrapper.classList.toggle('active');
-});
-signInBtnLink.addEventListener('click', () => {
-    wrapper.classList.toggle('active');
-});
+document.addEventListener('DOMContentLoaded', function () {
+    const signInBtnLink = document.querySelector('.signInBtn-link');
+    const signUpBtnLink = document.querySelector('.signUpBtn-link');
+    const wrapper = document.querySelector('.wrapper');
 
+    signUpBtnLink.addEventListener('click', () => {
+        wrapper.classList.toggle('active');
+    });
+
+    signInBtnLink.addEventListener('click', () => {
+        wrapper.classList.toggle('active');
+    });
+});
